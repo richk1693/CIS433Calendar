ef9c6d2989e5dc1586ec3625ad4869e139e38921
richk1693 <richk1693@gmail.com>
2015-11-18 21:08:52 -0500

    test

--- src/Login2.java
+++ src/Login2.java
@@ -56,90 +56,58 @@ public class Login2 extends Application {
 		primaryStage.setTitle("433 Calendar Login");
 		BorderPane bp = new BorderPane();
 		bp.setPadding(new Insets(10, 50, 50, 50));
+		
 		// Adding HBox
-
 		HBox hb = new HBox();
-
 		hb.setPadding(new Insets(20, 20, 20, 30));
 
 		// Adding GridPane
-
 		GridPane gridPane = new GridPane();
-
 		gridPane.setPadding(new Insets(20, 20, 20, 20));
-
 		gridPane.setHgap(5);
-
 		gridPane.setVgap(5);
 
 		// Implementing Nodes for GridPane
-
 		Label lblUserName = new Label("Username");
-
 		final TextField txtUserName = new TextField();
-
 		Label lblPassword = new Label("Password");
-
 		final PasswordField pf = new PasswordField();
-
 		Button btnLogin = new Button("Login");
-
 		final Label lblMessage = new Label();
 
 		// Adding Nodes to GridPane layout
-
 		gridPane.add(lblUserName, 0, 0);
-
 		gridPane.add(txtUserName, 1, 0);
-
 		gridPane.add(lblPassword, 0, 1);
-
 		gridPane.add(pf, 1, 1);
-
 		gridPane.add(btnLogin, 2, 1);
-
 		gridPane.add(lblMessage, 1, 2);
 
 		// Reflection for gridPane
-
 		Reflection r = new Reflection();
-
 		r.setFraction(0.7f);
-
 		gridPane.setEffect(r);
 
 		// DropShadow effect
-
 		DropShadow dropShadow = new DropShadow();
-
 		dropShadow.setOffsetX(5);
-
 		dropShadow.setOffsetY(5);
 
 		// Adding text and DropShadow effect to it
-
 		Text text = new Text("433 Calendar Login");
-
 		text.setFont(Font.font("Courier New", FontWeight.BOLD, 28));
-
 		text.setEffect(dropShadow);
 
 		// Adding text to HBox
-
 		hb.getChildren().add(text);
 
 		// Add ID's to Nodes
-
 		bp.setId("bp");
-
 		gridPane.setId("root");
-
 		btnLogin.setId("btnLogin");
-
 		text.setId("text");
 
 		// Action for btnLogin
-
 		btnLogin.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
@@ -173,27 +141,19 @@ public class Login2 extends Application {
 		});
 
 		// Add HBox and GridPane layout to BorderPane Layout
-
 		bp.setTop(hb);
-
 		bp.setCenter(gridPane);
 
 		// Adding BorderPane to the scene and loading CSS
-
 		Scene scene = new Scene(bp);
-
 		// scene.getStylesheets().add(
 		// getClass().getClassLoader().getResource("login.css")
 		// .toExternalForm());
 
 		primaryStage.setScene(mainScene());
-
 		primaryStage.titleProperty().bind(
-
 		scene.widthProperty().asString().
-
 		concat(" : ").
-
 		concat(scene.heightProperty().asString()));
 
 		// primaryStage.setResizable(false);
